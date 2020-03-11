41e3810568562afb2727c58ead8d76735f590f48
fewtrg <60783720+fewtrg@users.noreply.github.com>
2020-03-11 13:15:48 +0000

    beginning of wednesday

---
+++ BankingDomain/BankAccount.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace BankingDomain
+{
+    public class BankAccount
+    {
+        private decimal CurrentBalance = 7000; 
+       
+        public decimal GetBalance()
+        {
+            return CurrentBalance;  // to get a meaningful error unless it needs a config change
+        }
+
+        public void Withdraw(decimal amountToWithdraw)
+        {
+           if(amountToWithdraw >= CurrentBalance)
+            {
+                return;
+            }
+            CurrentBalance -= amountToWithdraw;
+        }
+
+        public void Deposit(decimal amountToDeposit)
+        {
+            CurrentBalance += amountToDeposit;
+        }
+    }
+}
---
+++ BankingTests/BankAccountTests.cs
@@ -0,0 +1,65 @@
+﻿using BankingDomain;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BankingTests
+{
+    public class BankAccountTests
+    {
+        private decimal amountToDeposit;
+
+        [Fact]
+        public void NewAccountHasCorrectBalance()
+        {
+            var account = new BankAccount();
+
+            decimal currentBalance = account.GetBalance();
+
+            Assert.Equal(7000M, currentBalance);
+        }
+
+        [Fact]
+        public void WithdrawalDecreasesBalance()
+        {
+            // Arrange - Given
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToWithdraw = 1M;
+            // Act - When
+            account.Withdraw(amountToWithdraw);
+            //Assert - then
+            Assert.Equal(
+                openingBalance - amountToWithdraw,
+                account.GetBalance());
+
+        }
+
+        [Fact]
+        public void DepositIncreasesBalance()
+        {
+            // Arrange - Given
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+            var amountToDeposit = 1M;
+            // Act - When
+            account.Deposit(amountToDeposit);
+            //Assert - then
+            Assert.Equal(
+                openingBalance + amountToDeposit,
+                account.GetBalance());
+        }
+
+        [Fact]
+        public void OverdraftDoesNotDecreaseBalance()
+        {
+            var account = new BankAccount();
+            var openingBalance = account.GetBalance();
+
+            account.Withdraw(openingBalance + 1);
+
+            Assert.Equal(openingBalance, account.GetBalance());
+        }
+    }
+}
